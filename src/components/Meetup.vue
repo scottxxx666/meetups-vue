07f94a6a2eea1fdb40c57f3d028eaf69cb6a415b
Scott Kao <scottxxx666@gmail.com>
2018-05-13 00:18:44 +0800

    Try vue router

---
+++ src/components/Meetup.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>Meetup</div>
+    <h3>
+      <router-link to="/">回首頁</router-link>
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Meetup',
+  data() {
+    return {
+      msg: 'TTTTTTTTTTTest',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/components/Home';
+import Meetup from '@/components/Meetup';
 
 Vue.use(Router);
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/meetup',
+      name: 'Meetup',
+      component: Meetup,
     },
   ],
 });
